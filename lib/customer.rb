3e7be940f0df1c1992ba14e5ae99a1d9a37718bf
Antonio Espinoza <aespinoza777@gmail.com>
2020-05-20 00:34:10 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,23 @@
 class Customer
-  
+  attr_accessor :name, :age
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+end
+
+def self.all
+@@all
+end
+
+def new_meal(waiter,total,tip)
+  meal = meal.new {|meal|meal.customer == self}
+end
+
+def waiters
+  customer.all.select {|meal| meal.waiter == self}
+end
+
 end
